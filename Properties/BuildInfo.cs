cb890e19cf135bcceb3765d577c7233a99ce06d9
MrSago <37180898+MrSago@users.noreply.github.com>
2024-12-19 18:03:04 +0300

    chore: add download link to build info

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -8,6 +8,6 @@ internal static class BuildInfo
     public const string COMPANY = null;
     public const string VERSION = "0.0.1";
     public const string GAMEVERSION = null;
-    public const string DOWNLOADLINK = null;
+    public const string DOWNLOADLINK = "https://github.com/MrSago/ModSide";
     public const string GUID = "9A19103F-16F7-4668-BE54-9A1E7A4F7556";
 }
