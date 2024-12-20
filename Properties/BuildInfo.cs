e8f7cc63b27a347ae7498c0dd6fe20fe8c32725d
MrSago <37180898+MrSago@users.noreply.github.com>
2024-12-21 02:38:52 +0300

    chore: version up

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -6,7 +6,7 @@ internal static class BuildInfo
     public const string DESCRIPTION = "Just for fun!";
     public const string AUTHOR = "MrS4g0";
     public const string COMPANY = null;
-    public const string VERSION = "0.0.1";
+    public const string VERSION = "0.0.2";
     public const string GAMEVERSION = null;
     public const string DOWNLOADLINK = "https://github.com/MrSago/MiSide-KappiMod";
     public const string GUID = "E90C66AE-9EDB-4981-A794-AA46B32DD4B9";
