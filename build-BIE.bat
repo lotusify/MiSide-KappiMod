7856322e2775c7901f48995481d72c48acbe232f
MrSago <37180898+MrSago@users.noreply.github.com>
2024-12-26 08:28:25 +0300

    refact: improve build scripts

--- build-BIE.bat
+++ build-BIE.bat
@@ -1,3 +1,19 @@
-dotnet build -c BIE
+@echo off
+setlocal EnableDelayedExpansion
 
-copy /Y ".\bin\Release\KappiMod.BepInEx.dll" "C:\Program Files (x86)\Steam\steamapps\common\MiSide\BepInEx\plugins\KappiMod\KappiMod.BepInEx.dll"
+dotnet build -c BIE
+if errorlevel 1 goto :error
+
+set "SOURCE=.\bin\Release"
+set "DEST=C:\Program Files (x86)\Steam\steamapps\common\MiSide\BepInEx\plugins\KappiMod"
+set "FILE=KappiMod.BepInEx.dll"
+
+robocopy "%SOURCE%" "%DEST%" "%FILE%" /NFL /NDL /NJH /NJS
+if errorlevel 8 goto :error
+
+echo Build and copy completed successfully
+exit /b 0
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
--- build-ML.bat
+++ build-ML.bat
@@ -1,3 +1,19 @@
-dotnet build -c ML
+@echo off
+setlocal EnableDelayedExpansion
 
-copy /Y ".\bin\Release\KappiMod.MelonLoader.dll" "C:\Program Files (x86)\Steam\steamapps\common\MiSide\Mods\KappiMod.MelonLoader.dll"
+dotnet build -c ML
+if errorlevel 1 goto :error
+
+set "SOURCE=.\bin\Release"
+set "DEST=C:\Program Files (x86)\Steam\steamapps\common\MiSide\Mods"
+set "FILE=KappiMod.MelonLoader.dll"
+
+robocopy "%SOURCE%" "%DEST%" "%FILE%" /NFL /NDL /NJH /NJS
+if errorlevel 8 goto :error
+
+echo Build and copy completed successfully
+exit /b 0
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
