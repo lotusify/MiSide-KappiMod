82fa185d54f70667e17a33810caa3362de2530ca
MrSago <37180898+MrSago@users.noreply.github.com>
2024-12-30 02:50:27 +0300

    feat: copy userlibs for build scripts

--- build-BIE.bat
+++ build-BIE.bat
@@ -8,7 +8,14 @@ set "SOURCE=.\bin\Release"
 set "DEST=C:\Program Files (x86)\Steam\steamapps\common\MiSide\BepInEx\plugins\KappiMod"
 set "FILE=KappiMod.BepInEx.dll"
 
+set "USERLIBS_SRC=.\Assemblies\UserLibs"
+set "USERLIBS_DEST=C:\Program Files (x86)\Steam\steamapps\common\MiSide\BepInEx\plugins\KappiMod"
+set "UNIVERSELIB=UniverseLib.BIE.IL2CPP.Interop.dll"
+
 robocopy "%SOURCE%" "%DEST%" "%FILE%" /NFL /NDL /NJH /NJS
+if errorlevel 8 goto :error
+
+robocopy "%USERLIBS_SRC%" "%USERLIBS_DEST%" "%UNIVERSELIB%" /NFL /NDL /NJH /NJS /XO
 if errorlevel 8 goto :error
 
 echo Build and copy completed successfully
--- build-ML.bat
+++ build-ML.bat
@@ -8,7 +8,14 @@ set "SOURCE=.\bin\Release"
 set "DEST=C:\Program Files (x86)\Steam\steamapps\common\MiSide\Mods"
 set "FILE=KappiMod.MelonLoader.dll"
 
+set "USERLIBS_SRC=.\Assemblies\UserLibs"
+set "USERLIBS_DEST=C:\Program Files (x86)\Steam\steamapps\common\MiSide\UserLibs"
+set "UNIVERSELIB=UniverseLib.ML.IL2CPP.Interop.dll"
+
 robocopy "%SOURCE%" "%DEST%" "%FILE%" /NFL /NDL /NJH /NJS
+if errorlevel 8 goto :error
+
+robocopy "%USERLIBS_SRC%" "%USERLIBS_DEST%" "%UNIVERSELIB%" /NFL /NDL /NJH /NJS /XO
 if errorlevel 8 goto :error
 
 echo Build and copy completed successfully
